e0197bae6b6da2e105ff83b578ec588ec1bff935
cameospachuau28 <cameospachuau28@gmail.com>
2018-05-22 17:52:58 +0530

    Updates

--- MSA/settings.py
+++ MSA/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'iun)lhs)zwq6yy(nh+$hi&btyg+f)#
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = [ 'msa-bangalore-college-list.herokuapp.com','127.0.0.1']
 
 
 # Application definition
